59bd14233380ff8d7f375f8c62cbfbf588b95aec
Hogara Okamoto <okamoto@100dreams.co.jp>
2024-09-09 07:45:12 +0900

    Base URL changed to http://127.0.0.1:5000/v1

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,7 +1,9 @@
 import OpenAI from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
-const openai = new OpenAI();
+const openai = new OpenAI({
+  baseURL: "http://127.0.0.1:5000/v1",
+});
 
 export const runtime = "edge";
 
@@ -9,7 +11,7 @@ export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const response = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
+    model: "gpt-3.5-turbo",
     stream: true,
     messages: [
       {
